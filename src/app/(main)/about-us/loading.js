11d5637786e2e82d4800b272802a193aa1db6140
Thegeekpod <supriyo7dey@gmail.com>
2024-09-24 00:17:53 +0530

    ss

---
+++ src/app/(main)/about-us/loading.js
@@ -0,0 +1,9 @@
+import { Loder } from "@/component/main/common/Loder";
+
+export default function Loading() {
+    // You can add any UI inside Loading, including a Skeleton.
+    return (
+   <Loder/>
+
+    )
+  }
---
+++ src/component/main/common/Loder.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Loder = () => {
+  return (
+    <div className="rounded-2xl bg-white p-6 shadow dark:bg-black dark:shadow-dark lg:col-span-2 lg:p-10 flex justify-center items-center">
+  <img src="/loder.webp" alt="loding" className="w-1/3" />
+</div>
+  )
+}
