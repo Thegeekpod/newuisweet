146116393f6db3a462773835a172250c209b6616
thegeekpod <supriyo7dey@gmail.com>
2025-01-10 01:19:49 +0530

    xx

---
+++ src/app/(main)/exter/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div class="w-full bg-gradient-to-r from-indigo-100 to-violet-100 text-gray-800 rounded-lg p-4 my-4"></div>
+  )
+}
+
+export default page
